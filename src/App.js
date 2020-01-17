4ea10fe71d548b9c7cf5e13355b06eaa474fd020
DavoAnt <dav-antonyan94@mail.ru>
2020-01-17 19:23:33 +0400

    arajin popoxutyun

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import GlobalStyles from './styles/GlobalStyles';
+import styled from 'styled-components';
+
+
+const Blok = styled.div`
+    ${({width}) => width ? `width: ${width};` : ''}
+    ${({height}) => height ? `height: ${height};` : ''}
+    ${({bgColor}) => bgColor ? `background-color: ${bgColor};` : ''}
+   
+`;
+
+const App = () => {
+    return (
+        <>
+            <GlobalStyles />
+            <h1>Hello!!!</h1>
+            <Blok width='170px' height='50px' bgColor='orange'></Blok> 
+            <Blok width='80px' height='100px' bgColor='violet'></Blok> 
+        </>
+        
+    )
+};
+
+export default App;
